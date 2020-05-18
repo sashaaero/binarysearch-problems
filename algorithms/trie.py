41285aed597125772e20a837d64f272054902aa5
Alexander Tischenko <sashaaero@yandex.ru>
2020-05-19 00:20:00 +0300

    Added trie data structure

---
+++ algorithms/trie.py
@@ -0,0 +1,8 @@
+def make_trie(words):
+    _end = '.'
+    trie = dict()
+    for word in words:
+        current_dict = trie
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
